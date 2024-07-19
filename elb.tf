6d3b53fb74880b9440aa43073d6a75a9777536a9
MARCOS EDUARDO DA SILVA SANTOS <marcoseduardoss@gmail.com>
2024-07-18 23:48:42 -0300

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,21 +1,63 @@
-resource "aws_elb" "web" {
-  name               = "elb-marcos-eduardo"
-  availability_zones = ["us-east-1a", "us-east-1b"]
+resource "aws_security_group" "web" {
+  name   = "allow_web_traffic"
+  vpc_id      = "vpc-f8709985"
 
-  listener {
-    instance_port     = 80
-    instance_protocol = "HTTP"
-    lb_port           = 80
-    lb_protocol       = "HTTP"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  health_check {
-    target              = "HTTP:80/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  instances = [aws_instance.instancia_1.id, aws_instance.instancia_2.id]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "alb" {
+  name               = "default-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = ["subnet-db701c96", "subnet-e4df06bb"]
+  security_groups    = [aws_security_group.allow_web.id]
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name     = "default-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id      = VPC.id
+}
+
+resource "aws_lb_target_group_attachment" "tg_ec2_attachment" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.instancia_1.id 
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "tg_ec2_attachment2" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.instancia_2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "default_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
 }
