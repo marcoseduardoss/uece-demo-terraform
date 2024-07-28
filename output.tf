5b9a3397fabd74a7dbe907ada25da7e041ac39fc
Marcos Eduardo <marcos.eduardo@boavistapartners.com.br>
2024-07-28 16:33:47 -0300

    create variables

--- output.tf
+++ output.tf
@@ -1,4 +1,3 @@
-
 output "final_snapshot_id" {
   value = aws_db_instance.meu_rds.final_snapshot_identifier
 }
--- rds.tf
+++ rds.tf
@@ -3,23 +3,16 @@ resource "aws_db_instance" "meu_rds" {
   engine                = "mysql"
   engine_version        = "8.0"
   instance_class        = "db.t3.micro"
-  db_name               = "MeuMysql" 
-  username              = "admin"
-  password              = "MinhaSenhaSegura123"
+  db_name               = var.db_name
+  username              = var.db_username
+  password              = var.db_password
   parameter_group_name  = "default.mysql8.0"
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
-  skip_final_snapshot   = true// Medar p/ false garante que o snapshot seja criado antes da deleção
+  skip_final_snapshot   = true
   final_snapshot_identifier = "meu-rds-snapshot-final-${formatdate("YYYYMMDDHHmmss", timestamp())}"
 
   tags = {
     Name = "MeuRDS"
   }
 }
-
-
-#VPC: vpc-f8709985 
-
-#Availability Zones:
-#subnet-db701c96 us-east-1a (use1-az4)
-#subnet-e4df06bb us-east-1b (use1-az6)
